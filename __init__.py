2cb75cd8c85c3b917e6e576aee4e3570dc7ae956
Dmitriy P <jumper047@gmail.com>
2022-07-28 02:07:04 +0300

    Improve button discovery

--- __init__.py
+++ __init__.py
@@ -351,18 +351,14 @@ class MqttAdapterSkill(MycroftSkill):
     def advertise_listen_button(self, discovery_prefix):
         id = self.mqtt_discovery_unique_id() + "listen_button"
         config = {
-            'name':'doorbell',
-            'unique_id':'doorbell_button',
-            'payload_press':'press',
-            'command_topic':'homeassistant/button/doorbell/commands'
-            # "command_topic": self.topics.listen_button,
-            # "name": "Mycroft Listen Command",
-            # "uniq_id": id, 
-            # "payload_press": "PRESS",
-            # "icon": "mdi:microphone-off",
-            # "device": self.mqtt_device_config()
+            "command_topic": self.topics.listen_button,
+            "name": "Mycroft Listen Command",
+            "uniq_id": id,
+            "payload_press": "PRESS",
+            "icon": "mdi:record-rec",
+            "device": self.mqtt_device_config()
         }
-        # config.update(self.mqtt_availability_config())
+        config.update(self.mqtt_availability_config())
         discovery_topic = "{}/button/{}/config".format(discovery_prefix, id)
         self.mqtt.publish(discovery_topic, payload=json.dumps(config), retain=True)
         self.log.info('Listen button advertised')
